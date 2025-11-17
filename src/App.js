80aafc7838eff730f82565e687f9468e8d64fa3f
Jordy <jordy.perret@gmail.com>
2025-11-17 23:42:31 +0100

    V1.1.3 - Fixed redirect to non existing pages

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/wp-admin" element={<Admin />} />
+        <Route path='*' element={<Homepage />} />
       </Routes>
       <Analytics />
     </Router>
